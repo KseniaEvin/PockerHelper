11af4934269e30d80a89f0c91acae230d47f4c53
Ksenia Evin <Ksenia.Evin@emergn.com>
2020-02-11 18:29:54 +0300

    setup module

---
+++ setup.js
@@ -0,0 +1,33 @@
+function prepareCardsBeforeGame() {
+    let deck = [];
+
+    const suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
+  
+    const ranks = [2, 3, 4, 5, 6, 7, 8,
+      9, 10, 11, 12, 13, 14]
+  
+    for (let i = 0; i < suits.length; i++) {
+      for (let j = 0; j < ranks.length; j++) {
+        let card = { suit: suits[i], rank: ranks[j] };
+        deck = [...deck, card];
+      }
+    }
+  
+    let fiveCards = [];
+  
+    function getRandomCard(max) {
+      return Math.floor(Math.random() * Math.floor(max));
+    }
+  
+  
+    while (fiveCards.length !== 5) {
+      let index = getRandomCard(deck.length);
+      fiveCards.push(deck[index]);
+      fiveCards = fiveCards.filter((value, index, fiveCards) => fiveCards.indexOf(value) === index);
+    }
+
+    return { deck, fiveCards };
+  
+};
+
+module.exports = prepareCardsBeforeGame;  
