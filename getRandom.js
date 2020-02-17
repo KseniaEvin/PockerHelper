f340abe30b593d968f78687f7375f4b8953943e8
Ksenia Evin <Ksenia.Evin@emergn.com>
2020-02-17 18:11:28 +0300

    updates

---
+++ getRandom.js
@@ -0,0 +1,28 @@
+let deck = [];
+
+const suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
+
+const ranks = [2, 3, 4, 5, 6, 7, 8,
+  9, 10, 11, 12, 13, 14]
+
+for (let i = 0; i < suits.length; i++) {
+  for (let j = 0; j < ranks.length; j++) {
+    let card = { suit: suits[i], rank: ranks[j] };
+    deck = [...deck, card];
+  }
+}
+
+console.log(deck);
+
+
+let fiveCards = [];
+let getRandom = function (deck) {
+for(let i = 0; i < 5; i++) {
+    let index = Math.floor(Math.random() * deck.length);
+    fiveCards = [...fiveCards, deck[index]];
+    deck.splice(index, 1);
+}
+};
+
+getRandom(deck);
+console.log(fiveCards);
