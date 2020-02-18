6ea2145a774c8bdd06b2af409b546b7f2401cd16
Ksenia Evin <Ksenia.Evin@emergn.com>
2020-02-18 16:27:13 +0300

    updates

--- game.js
+++ game.js
@@ -1,15 +1,19 @@
 const getRandomFiveCards = require('./getRandomFiveCards');
 const getDeck = require('./getDeck');
 const { flush, straight } = require('./checkCombinations');
+const { getSuits, getRanks } = require('./utilities');
 
 function game() {
    let deck = getDeck();
    let fiveCards = getRandomFiveCards(deck);
+   let suits = getSuits(fiveCards);
+   let ranks = getRanks(fiveCards);
    let isFlush = flush(fiveCards);
    let isStraight = straight(fiveCards);
-
    let isStraightFlush = isFlush && isStraight;
+   let isflushRoyal = isStraightFlush && ranks[0] === 10;
    console.log(deck);
+   console.log(fiveCards);
 }
 
 module.exports = game;
