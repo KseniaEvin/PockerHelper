aed797e22e976f8d4d4e659cfca6fcbb935080ea
Ksenia Evin <Ksenia.Evin@emergn.com>
2020-02-25 16:39:46 +0300

    create to tests

---
+++ src/straightFlushFalse.test.js
@@ -0,0 +1,10 @@
+let fiveCards =   [ { suit: 'Hearts', rank: 10 },
+                    { suit: 'Hearts', rank: 12 },
+                    { suit: 'Hearts', rank: 11 },
+                    { suit: 'Clubs', rank: 9 },
+                    { suit: 'Hearts', rank: 13 }];
+const straightFlush = require('./straightFlush');
+let isStraightFlush = straightFlush(fiveCards);
+test('case, when straightFlush should return true', () => {
+    expect(isStraightFlush).toBeFalsy();
+});
---
+++ src/straightFlushTrue.test.js
@@ -0,0 +1,10 @@
+let fiveCards =   [ { suit: 'Hearts', rank: 10 },
+                    { suit: 'Hearts', rank: 12 },
+                    { suit: 'Hearts', rank: 11 },
+                    { suit: 'Hearts', rank: 9 },
+                    { suit: 'Hearts', rank: 8 }];
+const straightFlush = require('./straightFlush');
+let isStraightFlush = straightFlush(fiveCards);
+test('case, when straightFlush should return true', () => {
+    expect(isStraightFlush).toBeTruthy();
+});
