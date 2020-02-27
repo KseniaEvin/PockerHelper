c7558f6330ba8fa3164ef2ed84877b439d042388
Ksenia Evin <Ksenia.Evin@emergn.com>
2020-02-27 14:13:01 +0300

    made random cards

--- src/getRandomSevenCards.js
+++ src/getRandomSevenCards.js
@@ -1,18 +1,10 @@
 let getRandomSevenCards = function (deck) {
-    let sevenCards = [
-        { suit: 'Diamonds', rank: 2 },
-  { suit: 'Clubs', rank: 4 },
-  { suit: 'Spades', rank: 2 },
-  { suit: 'Hearts', rank: 4 },
-  { suit: 'Diamonds', rank: 10 },
-  { suit: 'Diamonds', rank: 4 },
-  { suit: 'Spades', rank: 4 }
-    ];
-    // for (let i = 0; i < 7; i++) {
-    //     let index = Math.floor(Math.random() * deck.length);
-    //     sevenCards = [...sevenCards, deck[index]];
-    //     deck.splice(index, 1);
-    // };
+    let sevenCards = [];
+    for (let i = 0; i < 7; i++) {
+        let index = Math.floor(Math.random() * deck.length);
+        sevenCards = [...sevenCards, deck[index]];
+        deck.splice(index, 1);
+    };
     
     return sevenCards;
 };
