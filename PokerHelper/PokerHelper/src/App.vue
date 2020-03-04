05440b21d49413ae47c7191346f48dd9b372c7a8
Ksenia Evin <Ksenia.Evin@emergn.com>
2020-03-04 15:47:11 +0300

    created separate vue component with webpack

---
+++ PokerHelper/PokerHelper/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col col-2 border border-dark card" v-for="card in sevenCards" :class="{red: card.suit==='Diamonds' || card.suit==='Hearts'}">{{card.suit}} <br> {{card.rank}}</div>
+        </div>
+      </div>
+</template>
+
+<script>
+export default {
+    data() {
+            return {
+              sevenCards: [ { suit: 'Diamonds', rank: 10 },
+                { suit: 'Hearts', rank: 12 },
+                { suit: 'Hearts', rank: 11 },
+                { suit: 'Hearts', rank: 14 },
+                { suit: 'Clubs', rank: 13 },
+                { suit: 'Hearts', rank: 14 },
+                { suit: 'Spades', rank: 13 } ]
+              } 
+          }
+}
+</script>
